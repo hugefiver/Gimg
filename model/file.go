edffe692a8b26905704daa0ee155bf12484e198d
hugefiver <hugefiver@outlook.com>
2019-07-14 16:26:58 +0800

    add models: image,file

--- model/file.go
+++ model/file.go
@@ -1,1 +1,9 @@
 package model
+
+import "github.com/jinzhu/gorm"
+
+type File struct {
+    gorm.Model
+    Md5 string `gorm:"size:32;primary_key"`
+    Data []byte `gorm:"not null"`
+}
--- model/image.go
+++ model/image.go
@@ -1,1 +1,11 @@
 package model
+
+import "time"
+
+type Image struct {
+    ImgId string `gorm:"size:20;primary key" sql:"index"`
+    Ext string `gorm:"size:10;not null"`
+    UploadTime time.Time `gorm:"size:"`
+    Uploader string
+    File File
+}
