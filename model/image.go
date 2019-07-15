74c573afd0aaf1c295a9da352f274d568d1591b4
hugefiver <hugefiver@outlook.com>
2019-07-15 12:15:37 +0800

    add models: image,file

--- model/image.go
+++ model/image.go
@@ -5,7 +5,7 @@ import "time"
 type Image struct {
     ImgId string `gorm:"size:20;primary key" sql:"index"`
     Ext string `gorm:"size:10;not null"`
-    UploadTime time.Time `gorm:"size:"`
+    UploadTime time.Time
     Uploader string
     File File
 }
